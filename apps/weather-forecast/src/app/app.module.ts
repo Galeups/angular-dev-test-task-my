46a8fa3408f2c2a23f9c05adfa61e2f25baaaecd
Galeups <artdevice@gmail.com>
2022-04-10 02:03:07 +0200

    feat(table): Delete line of table

--- apps/weather-forecast/src/app/app.module.ts
+++ apps/weather-forecast/src/app/app.module.ts
@@ -6,11 +6,10 @@ import { WeatherForecastServicesModule } from '@bp/weather-forecast/services';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { TableComponent } from './components/table/table.component';
-import { LineComponent } from './components/line/line.component';
 import { ChangeModeComponent } from './components/change-mode/change-mode.component';
 
 @NgModule({
-	declarations: [AppComponent, SearchComponent, TableComponent, LineComponent, ChangeModeComponent],
+	declarations: [AppComponent, SearchComponent, TableComponent, ChangeModeComponent],
 	imports: [BrowserModule, WeatherForecastServicesModule, ReactiveFormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
--- apps/weather-forecast/src/app/components/line/line.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'bp-line',
-	templateUrl: './line.component.html',
-	styleUrls: ['./line.component.scss'],
-})
-export class LineComponent {}
