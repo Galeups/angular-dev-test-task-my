e6587632bb30cea833f38f6e760027e4a6f14d97
Galeups <artdevice@gmail.com>
2022-04-10 14:11:35 +0200

    feat(table): Styling the table

--- apps/weather-forecast/src/app/components/table/table.component.ts
+++ apps/weather-forecast/src/app/components/table/table.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { StateTableService } from '../../services/state-table.service';
+import { tap } from 'rxjs';
 
 @Component({
 	selector: 'bp-table',
@@ -7,17 +8,17 @@ import { StateTableService } from '../../services/state-table.service';
 	styleUrls: ['./table.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TableComponent implements OnInit {
-	// private readonly _header = {
-	// 	daily: ['City Name', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-	// 	hourly: ['City Name', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00', '24:00'],
-	// };
-	constructor(private readonly _cdr: ChangeDetectorRef, private readonly _stateTable: StateTableService) {}
-
-	ngOnInit() {
-		this._stateTable.state$.subscribe(state => {
+export class TableComponent {
+	isShow = false;
+	readonly state$ = this._stateTable.state$.pipe(
+		tap(state => {
+			this.isShow = !!state.cities.length;
 			console.log(state);
-			this._cdr.markForCheck();
-		});
-	}
+		})
+	);
+	readonly header = {
+		daily: ['City Name', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+		hourly: ['City Name', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00', '24:00'],
+	};
+	constructor(private readonly _stateTable: StateTableService) {}
 }
